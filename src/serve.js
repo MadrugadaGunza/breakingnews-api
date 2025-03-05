3dddc90d3d15077c515b27dbe288ac948403bd67
Madrugada de Carvalho <madrugadaoca@gmail.com>
2025-03-05 08:06:34 +0100

    feat: add router user

--- src/serve.js
+++ src/serve.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import databaseConnect from './database/db.js';
+import userRouter from './routes/userRouter.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 databaseConnect();
+
+app.use('api/user', userRouter);
 
 app.listen(PORT, () => console.log(`Servidor rodadndo na porta: ${PORT}`));
